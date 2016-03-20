116fd1d0e08722a397f73aa026a324d3c4d3584f
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2016-03-20 17:23:17 +0100

    Installation script installs plugins from plugin_file

--- install_atom.sh
+++ install_atom.sh
@@ -10,72 +10,61 @@
 #   Script to manage atom editor installation and plugin
 #   downloading.
 # Usage:
-#
+#   bash install_atom.sh
 # -------------------------------------------------------
 # Script:
+
+separator(){
+  echo "-----------------------------------------------------------------------"
+  echo ""
+}
 
 # Download atom editor
 download_atom(){
   echo "--- Downloading atom editor ---"
-  echo "-------------------------------"
+  separator
   wget https://atom.io/download/deb -O atom_editor.deb
 }
 
 # Install atom from the downloaded file
 install_atom(){
   echo "--- Installing atom editor ---"
-  echo "------------------------------"
+  separator
   sudo dpkg -i atom_editor.deb
 }
 
 # Remove the downloaded file
 remove_file(){
   echo "--- Removing the downloaded file ---"
-  echo "------------------------------------"
+  separator
   rm atom_editor.deb
 }
 
-# Navigate to atom directory
-go_to_atom_dir(){
-  cd ~/.atom
+# Copy conf files to atom directory
+copy_conf_files(){
+  echo "--- Setting up conf files ---"
+  separator
+  cp config.cson keymap.cson snippets.cson styles.less ~/.atom
 }
 
-# Download the conf files from git
-download_conf_files(){
-  echo "--- Downloading the conf files ---"
-  echo "----------------------------------"
-  git clone https://github.com/miroslavvidovic/atom-configuration
-}
-
-# Replace the default conf files
-replace_conf_files(){
-  cp atom-configuration/* .
-}
-
-# Install plugins using the built in atom package manager
-# Plugins are stored in an array and for each element in
-# the array run the install command
-# TODO: Add  UI themes and colorschemes to the installation
-# TODO: Move plugin list to a file
-install_plugins(){
+# Install plugins from text file
+install_plugins_from_file(){
   echo "--- Installing plugins/packages ---"
-  echo "-----------------------------------"
-  plugins=( autoclose-html color-picker emmet file-icons git-log git-plus git-status highlight-selected linter minimap todo-show )
-  for plugin in ${plugins[@]}; do
-    apm install $plugin
-  done
+  separator
+cat plugins_file.txt | while read plugin
+do
+  apm install $plugin
+done
 }
 
-main_program(){
+main_function(){
   download_atom
   install_atom
   remove_file
-  go_to_atom_dir
-  download_conf_files
-  replace_conf_files
-  install_plugins
+  copy_conf_files
+  install_plugins_from_file
 }
 
-main_program
+main_function
 
 exit 0
